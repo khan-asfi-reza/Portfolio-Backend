8a47b912221593e503b51796f61728a42071c900
Khan Asfi Reza <khanasfireza10@gmail.com>
2021-05-11 03:10:07 +0600

    Added Static Files

--- KhanAsfiReza/settings.py
+++ KhanAsfiReza/settings.py
@@ -11,8 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+
 from KhanAsfiReza.env import *
 
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
